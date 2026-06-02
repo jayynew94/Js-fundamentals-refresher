bbbda940ca810eddf14d8a68e692c7222cd66a49
Jeremy New <codingmachine@Jeremys-MacBook-Pro.local>
2026-06-01 19:57:58 -0500

    Day 1: variables and data types

---
+++ Practice.js
@@ -0,0 +1,15 @@
+const name = "Jay";
+const age = 33;
+const isHired = false;
+let nothing = null;
+let undef = undefined;
+
+console.log(typeof name); // string
+console.log(typeof age); // number
+console.log(typeof isHired); // boolean
+console.log(typeof nothing); // object
+console.log(typeof undef); // undefined
+
+//type conversion
+console.log(String(age));
+console.log(Number(33));
